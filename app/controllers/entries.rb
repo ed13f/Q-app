69c1bd0c425e148a0ad1405077d40f447f3cdfe3
Enrico De Santis <email": null>
2017-08-30 11:20:26 -0400

    Refactor to comply with rest

--- app/controllers/entries.rb
+++ app/controllers/entries.rb
@@ -1,40 +1,40 @@
-get '/all-the-entries' do
+get '/entries' do
   @entries = Entry.order(:created_at)
   erb :'entries/index'
 end
 
-get '/new-entry-form' do
+get '/entries/new' do
   erb :'entries/new'
 end
 
-post '/create-new-post' do
+post '/entries' do
   @entry = Entry.new(params[:entry])
   if @entry.save
-    redirect "/show-one-entry/#{@entry.id}"
+    redirect "/entries/#{@entry.id}"
   else
     @errors = @entry.errors.full_messages
     erb :'entries/new'
   end
 end
 
-get '/show-one-entry/:id' do
+get '/entries/:id' do
   @entry = Entry.find(params[:id])
   erb :'entries/show'
 end
 
-get '/edit-one-entry-form/:id' do
+get '/entries/:id/edit' do
   @entry = Entry.find(params[:id])
   erb :'entries/edit'
 end
 
-post '/update-entry/:id' do
+put '/entries/:id' do
   entry = Entry.find(params[:id])
   entry.update_attributes(params[:entry])
-  redirect "/show-one-entry/#{entry.id}"
+  redirect "/entries/#{@entry.id}"
 end
 
-get '/delete-entry/:id' do
+delete '/entries/:id' do
   entry = Entry.find(params[:id])
   entry.destroy
-  redirect '/all-the-entries'
+  redirect '/entries'
 end
--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,3 @@
 get '/' do
-  redirect '/all-the-entries'
+  redirect '/entries'
 end
